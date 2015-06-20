44d522db75b83206c64eeb803877fda8c08f87ac
Rodrigo Bravo <rodri.brv@gmail.com>
2015-06-20 17:44:53 -0430

    Validaciones y Traducciones al formulario

--- pyramid_form/forms.py
+++ pyramid_form/forms.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
-from wtforms import Form, TextField, TextAreaField, validators
+from wtforms import TextField, TextAreaField, IntegerField, validators
+from wtforms.ext.i18n.form import Form
 
 
 strip_filter = lambda x: x.strip() if x else None
 
 class PersonCreateForm(Form):
-    nombre = TextField('Nombre', [validators.Length(min=1, max=200)],
+    nombre = TextField('Nombre',
+                       [validators.Length(min=2, max=100), validators.required()],
                        filters=[strip_filter],
                        description="Solo su nombre Ej. Rodrigo ")
-    apellido = TextField('Apellido', [validators.Length(min=1, max=200)],
-                       filters=[strip_filter],
-                       description="Solo su nombre. Ej. Bravo")
-    correo = TextField('Correo Electrónico', [validators.Length(min=5, max=200)],
+    apellido = TextField('Apellido',
+                        [validators.Length(min=2, max=100), validators.required()],
+                        filters=[strip_filter],
+                        description="Solo su nombre. Ej. Bravo")
+    correo = TextField('Correo Electrónico',
+                       [validators.Length(min=5, max=120), validators.required(), validators.Email()],
                        filters=[strip_filter],
                        description="Ej. email@correo.com")
-    telefono = TextField('Teléfono', [validators.Length(min=10, max=20)],
-                       filters=[strip_filter],
-                       description="Debe ser solo números Ej. 4585552277")
-    direccion = TextAreaField('Dirección', [validators.Length(min=1)],
-                       filters=[strip_filter])
+    telefono = IntegerField('Teléfono', [validators.optional()],
+                        description="Debe ser solo números Ej. 4585552277")
+    direccion = TextAreaField('Dirección', [validators.optional()],
+                        filters=[strip_filter])
